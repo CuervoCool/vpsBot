4a79e5c76c0753f8a66734b0ac1ee73d0e0e1a20
CuervoCool <lalopiton065@gmail.com>
2023-12-11 17:11:31 -0600

    xd

--- VPSBot.sh
+++ VPSBot.sh
@@ -1,34 +1,9 @@
 #!/bin/bash
-
-#CIDdir=/etc/ADM-db && [[ ! -d ${CIDdir} ]] && mkdir ${CIDdir}
-SRC="sources" && [[ ! -d ${SRC} ]] && mkdir ${SRC}
-# CID="${CIDdir}/User-ID" && [[ ! -e ${CID} ]] && echo > ${CID}
-[[ $(dpkg --get-selections|grep -w "jq"|head -1) ]] || apt-get install jq -y &>/dev/null
-#[[ ! -e "/bin/ShellBot.sh" ]] && wget -O /bin/ShellBot.sh https://raw.githubusercontent.com/rudi9999/TeleBotGen/master/ShellBot.sh &> /dev/null
-[[ -e /etc/texto-bot ]] && rm /etc/texto-bot
-LINE="==========================="
-
-# Importando API
-source ShellBot.sh
-source ${SRC}/menu
-source ${SRC}/ayuda
-source ${SRC}/cache
-source ${SRC}/invalido
-source ${SRC}/status
-source ${SRC}/reinicio
-source ${SRC}/myip
-source ${SRC}/id
-source ${SRC}/comandos
-source ${SRC}/update
-source ${SRC}/comandline
-
-# Token del bot
-bot_token="$(cat token)"
-
-# Inicializando el bot
-ShellBot.init --token "$bot_token" --monitor --return map
-ShellBot.username
-
+[[ ! -e "./admin" ]] && read -p "id: " id;echo -e ${id} >> Admin-ID
+[[ ! -e "./token" ]] && read -p "token: " token;echo -e ${token} >> token
+	source ShellBot.sh && source pato.sh
+bot_token="$(cat token)" && CID="User-ID"
+ShellBot.init --token "$bot_token" --monitor --return map && ShellBot.username
 download_file () {
 # shellbot.sh editado linea 3986
 user=User-ID
